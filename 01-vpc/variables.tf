0411b0ebfa13307c6e508cc6b7c2f0148ae7dba2
swethakanala <swetha.bim@gmail.com>
2025-01-21 21:57:44 -0500

    created vpc_cidr variables

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -9,3 +9,9 @@ variable "project_name" {
   type        = string
   default     = "example-project"
 }
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}

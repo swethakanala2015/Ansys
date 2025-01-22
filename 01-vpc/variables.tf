127eff66caa9478b017d54012f7cc7c3a6b1d7fa
swethakanala <swetha.bim@gmail.com>
2025-01-21 21:58:56 -0500

    created private_subnet_cidr variables

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -21,3 +21,9 @@ variable "public_subnet_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
+
+variable "private_subnet_cidr" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}

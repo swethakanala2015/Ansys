0a1a472381ac21b819bbe15c389f36b361504ed6
swethakanala <swetha.bim@gmail.com>
2025-01-21 22:31:23 -0500

    created allow_all_egress variable

--- 02-sg/variable.tf
+++ 02-sg/variable.tf
@@ -39,3 +39,10 @@ variable "http_cidr_blocks" {
   type        = list(string)
   default     = ["0.0.0.0/0"]
 }
+
+
+variable "allow_all_egress" {
+  description = "Allow all outbound traffic (true/false)"
+  type        = bool
+  default     = true
+}

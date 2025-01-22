a9a86d65146904e9e21bb50140b976d92f0c42ab
swethakanala <swetha.bim@gmail.com>
2025-01-21 21:57:16 -0500

    created project variables

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -3,3 +3,9 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "project_name" {
+  description = "The project name for tagging resources"
+  type        = string
+  default     = "example-project"
+}
